dd1ebad7d12467c3bd3cc6d3524747d036e82cb2
Joshua T. Guerin <jguerin@utm.edu>
2021-02-16 19:17:43 -0600

    Now with tasty arguments.

--- Subset-Sum/scripts/generate.py
+++ Subset-Sum/scripts/generate.py
@@ -1,12 +1,21 @@
-import sys
+#!/usr/bin/env python3
+
+import argparse
 from random import randint
 
-upper_limit = 20
-set_size = 10
+# Process arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', default=100, type=int,
+                    help="Upper limit on integer size, non-inclusive. (Default: 100)")
+parser.add_argument('-s', default=100, type=int,
+                    help="Set size: number of values generated. (Default: 100)")
+args = parser.parse_args()
 
+upper_limit = args.u
+set_size = args.s
 
+# Generate value(). predicate for given parameters.
 randint(1, upper_limit)
-
 print("value(", end="")
 for i in range(1, set_size):
     print(randint(1, upper_limit), " ; ", sep="", end="")
