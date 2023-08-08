1610320b77599019ad02523a289546de160f7125
Joshua T. Guerin <jguerin@utm.edu>
2023-08-08 18:38:58 -0500

    Preliminary n-queens solution and printer. Need final constraint (diagonal).

---
+++ N-Queens/validate/print.py
@@ -0,0 +1,42 @@
+# Use:
+# clingo n_queens.lp | python3 ./validate/print.py
+# to be ran in previous directory where n_queens.lp is
+
+
+def printGrid(coords, n):
+    grid = []
+    for i in range(n):
+        grid.append([' ']*n)
+
+    for (a, b) in coords:
+        print(a, b)
+        grid[a-1][b-1] = 'Q'
+        
+    for g in grid:
+        print(g)
+
+toks = input().split()
+
+while not toks[0].startswith("Answer:"):
+    toks = input().split()
+
+toks = input().split()
+
+bound = 0
+
+solution = []
+
+for t in toks:
+    (fst, snd) = t.split(',')
+    fst = int(fst[7:])
+    snd = int(snd[:-1])
+    #print(fst, snd)
+
+    bound = fst if fst>bound else bound
+    bound = snd if snd>bound else bound
+    
+    solution.append((fst, snd))
+
+#print(solution)
+
+printGrid(solution, bound)
