2b53be3b97d62a1aa29f76597e6a62f5d832f6e2
Dave <dgjsua@gmail.com>
2025-05-27 18:19:52 -0600

    Changed perfect_numbers.lp to output the divisors for each perfect number. Changed its printing script to reflect this change.

--- Perfect-Numbers/print/print.py
+++ Perfect-Numbers/print/print.py
@@ -1,7 +1,11 @@
 # Use:
 # clingo perfect_numbers.lp -c n=<max> | python3 print/print.py
 #
-# To be ran in previous directory where prime.lp and composite.lp are.
+# To be ran in previous directory where perfect_numbers.lp is.
+
+
+import collections
+import re
 
 
 toks = input().split()
@@ -15,8 +19,19 @@ if len(toks) == 0:
     print("None found.")
 
 else:
-    nums = [int(t[8:-1]) for t in toks]
+    num_dict = collections.defaultdict(list)
     
-    nums.sort()
+    for t in toks:
+        n = re.findall(r'\d+', t)
 
-    print("Perfect Numbers: " + ", ".join(str(n) for n in nums))
+        if len(n) == 2:
+            num_dict[int(n[1])].append(int(n[0]))
+
+    nums = sorted(num_dict.items())
+
+    print("Perfect Numbers:")
+
+    spacing = '                 '
+    for item in nums:
+        print('%20d = %s' % (item[0], ' + '.join(str(i) for i in item[1])))
+    
