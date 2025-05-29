d17c46e6b125a3c6357c4b8aeeaf2e022d68ef9e
Jackson Madsen <mads1158@bears.unco.edu>
2025-05-29 13:59:21 -0600

    updated subset sum description

--- Subset-Sum/gen/generate.py
+++ Subset-Sum/gen/generate.py
@@ -1,3 +1,16 @@
+# File: generate.py
+# Author: Joshua Guerin
+
+# Description: Set generator for subset sum problem solver
+# Use: python3 generate.py -u u -s s > filename.lp
+#	where: u is the upper limit (non-inclusive) for the values of the set
+#	       s is the size of the set
+#	       filename.lp is the instance file to write to
+#	Each of s and u are optional (defaults u=100, s=100)
+#	The redirect (> filename.lp) can be omitted (to print to stdout)
+
+
+
 import argparse
 from random import randint
 
