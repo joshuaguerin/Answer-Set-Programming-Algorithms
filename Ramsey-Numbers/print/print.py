c1acd2ef9413a05800a478e354812e1d5be1c8f2
Joshua T. Guerin <jtguerin@gmail.com>
2026-02-12 11:09:12 -0700

    Updated the printer to interpret new predicates and add better coloring.

--- Ramsey-Numbers/print/print.py
+++ Ramsey-Numbers/print/print.py
@@ -26,24 +26,49 @@ toks = raw[index].split()
 edges = []
 red = []
 blue = []
+clique = []
 
+# Collect relevant data.
 for t in toks:
-    if t[0:4] == "edge":
-        (fst, snd) = t.split(',')
-        fst = int(fst[5:])
-        snd = int(snd[0:-1])
-        if fst < snd:
-            edges.append((fst, snd))
-    # clique
-    else:
-        (fst, snd) = t.split(',')
-        fst = int(fst[7:])
-        snd = snd[0:-1]
-        if snd=="red":
-            red.append(fst)
-        else:
-            blue.append(fst)
+    if t[0:4] == "colo" and ',' in t:
+        # Edge color
+            (fst, snd, color) = t.split(',')
+            #print((fst, snd, color))
+            fst = int(fst[6:])
+            snd = int(snd)
+            color = color[0:-1]
+            if fst < snd:
+                edges.append((fst, snd, color))
+    elif t[0:3] == "red":
+        t = int(t[4:-1])
+        red.append(t)
+        #print("red", t)
+    elif t[0:4] == "blue":
+        t = int(t[5:-1])
+        blue.append(t)
+        #print("blue", t)
+                
+        #print((fst, snd, color))
+    # # color
+    # elif t[0:4] == "colo":
+    #     (fst, snd) = t.split(',')
+    #     fst = int(fst[6:])
+    #     snd = snd[0:-1]
+    #     if snd=="red":
+    #         red.append(fst)
+    #     else:
+    #         blue.append(fst)
+    
+    #     # clique
+    # elif t[0:4] == "cliq":
+    #     (fst, snd) = t.split(',')
+    #     fst = int(fst[7:])
+    #     snd = snd[0:-1]
+        
+    #     clique.append(fst)
 
+#print(red, blue)
+    
 # Print to graphviz format.
 header = '''
 graph {
@@ -52,18 +77,31 @@ graph {
 print(header)
 
 for b in blue:
-    print("   ", b, "[color=\"blue\"];")
+    if not b in red:
+        print("   ", b, "[color=\"dodgerblue3\", fontcolor=\"white\", style=\"filled\"];")
+    else:
+        #print("   ", b, "[color=\"darkviolet\", style=\"filled\"];")
+        print("   ", b, "[color=\"dodgerblue3:firebrick3\", fontcolor=\"white\", style=\"filled\"];")
 
 for r in red:
-    print("   ", r, "[color=\"red\"];")
+    if not r in blue:
+        print("   ", r, "[color=\"red\", fontcolor=\"white\", style=\"filled\"];")
+        
+# for r in red:
+#     print("   ", r, "[color=\"red\"];")
 
-for(fst, snd) in edges:
-    if fst in blue and snd in blue:
-        print("   ", fst, "--", snd, "[color=\"blue\"]", sep='')
-    elif fst in red and snd in red:
-        print("   ", fst, "--", snd, "[color=\"red\"]", sep='')
+for(fst, snd, color) in edges:
+    if color == "blue":
+        color = "dodgerblue3"
     else:
-        print("   ", fst, "--", snd, "[color=\"black\"]", sep='')
+        color = "firebrick3"
+        
+    if fst in blue and snd in blue or fst in red and snd in red:
+        print("   ", fst, "--", snd, "[color=\"", color, "\", penwidth=3]", sep='')
+    else:
+        print("   ", fst, "--", snd, "[color=\"", color, "\"]", sep='')
+    
+
 
 footer= '''
 }
