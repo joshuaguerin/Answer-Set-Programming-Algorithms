bca584074460e7a3d2c7a94d35d90fa2dd1ce915
Joshua T. Guerin <jtguerin@gmail.com>
2026-04-02 13:48:41 -0600

    Most of the way through image generation.

--- Image-Processing/gen/generate.py
+++ Image-Processing/gen/generate.py
@@ -11,8 +11,34 @@ try:
 except FileNotFoundError:
     print("Error:", argv[1], "not found.", file=stderr)
     exit()
-    
+
+image = []
+
+# Read, pre-process image
 for line in image_in:
-    print(line)
+    line = line.strip()
+    line = line.split()
+    
+    image.append(line)
 
 image_in.close()
+    
+# Flatten the image
+image = [entry for line in image for entry in line]
+
+# Grab the header information
+(magic_num, cols, rows, depth) = image[:4]
+
+# Color information only
+image = image[4:]
+
+## Start Reporting Values:
+
+# Header
+print("type(", magic_num, ").", sep="")
+print("cols(", cols, ").", " rows(", rows, ").", sep="")
+print("depth(", depth, ").", sep="")
+
+for i in range(0, len(image), 3):
+    print("pixel(", "row, ", "col, ", image[i], ' ', image[i+1], ' ', image[i+2], ").", sep="")
+
