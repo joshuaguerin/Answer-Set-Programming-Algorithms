b9e837077664f2b1a17aa30a4e8a0a61f45b02df
Joshua T. Guerin <jtguerin@gmail.com>
2026-06-17 09:25:43 -0600

    Update to printer almost done--converted over to plain text and removed the additional file dependency.

--- Satisfiability/print/print.py
+++ Satisfiability/print/print.py
@@ -9,32 +9,8 @@
 #      where instance.lp contains clause predicates.
 # Notes:
 
-import argparse
 
-def clause_literal_to_html(literal: int) -> str:
-    html_str = f'x<sub>{abs(literal)}</sub>'
-    if literal < 0:
-        html_str = '&not;' + html_str
-    return html_str
-
-def main(args):
-    # Parse instance
-    instance = {}
-    literals = set()
-    with open(args.instance, 'r') as fin:
-        lines = fin.read().split('\n')
-        for line in lines:
-            line = line.strip()
-            if not line.startswith('clause'):
-                continue
-            clause, literal = line[7:].strip(').').split(',')
-            clause = int(clause)
-            literal = int(literal)
-            if clause not in instance:
-                instance[clause] = []
-            instance[clause].append(literal)
-            literals.add(abs(literal))
-    # Parse SAT/MAX-SAT output
+def main():
     solution = {}
     clingo_out = []
     clingo_out.append(input())
@@ -60,33 +36,14 @@ def main(args):
             elif assign.startswith('false'):
                 solution[int(assign[6:].strip(')'))] = False
 
-    # HTML Document preamble
-    preamble = \
-        """
-<html>
-  <header>
-    <style>
-        .satisfied {color: red;}
-        .unsatisfied {color: black;}
-        .sat-highlight {background-color: #99ff99;}
-    </style>
-  </header>
-  <body>
-        """
-    epilogue = \
-        """
-  </body>
-</html>
-        """
-    print(preamble)
-    # Render and output html for True/False literals
+
+    # Print True and False assigned literals.
     if solution is None:
         # Unsatisfiable
-        print("    <h1> UNSATISFIABLE </h1>")
+        print("UNSATISFIABLE")
+        exit()
     else:
         # Satisfiable
-        print("    <h1> Truth Assignment </h1>")
-        print("    <hr>")
         true_literals = []
         false_literals = []
         for literal in sorted(solution.keys()):
@@ -94,58 +51,18 @@ def main(args):
                 true_literals.append(literal)
             else:
                 false_literals.append(literal)
-        print("    <p>")
-        print("    <h3> True Literals </h3>")
-        print("    <hr>")
-        print("    <ul>")
+
+        # Print valuation
+        print("True  literals: ", end='')
         for literal in true_literals:
-            print(f"    <li> x<sub>{literal}</sub> </li>")
-        print("    </ul>")
-        print("    <p>")
-        print("    <h3> False Literals </h3>")
-        print("    <hr>")
-        print("    <ul>")
+            print("x", literal, sep='', end=' ')
+        print()
+
+        print("False literals: ", end='')
         for literal in false_literals:
-            print(f"    <li> x<sub>{literal}</sub> </li>")
-        print("    </ul>")
+            print("x", literal, sep='', end=' ')
+        print()
 
-        # Render and output html for boolean formula w/ colored satisfied
-        print("    <p><p><h1> Instance CNF Formula </h1>")
-        print("    <hr><p>")
-        clause_strs = []
-        sat_count = 0
-        # Render each clause
-        for clause in sorted(instance.keys()):
-            satisfied = False
-            literals = []
-            for literal in instance[clause]:
-                if (literal > 0 and abs(literal) in true_literals) or \
-                   (literal < 0 and abs(literal) in false_literals):
-                    satisfied = True
-                    lit_str = "<span class=satisfied>" + \
-                              clause_literal_to_html(literal) + \
-                              "</span>"
-                else:
-                    lit_str = "<span class=unsatisfied>" + \
-                              clause_literal_to_html(literal) + \
-                              "</span>"
-                literals.append(lit_str)
-            clause_str = "(" + " &or; ".join(literals) + ")"
-            if satisfied:
-                sat_count += 1
-                clause_str = "<span class=sat-highlight>" + \
-                             clause_str + \
-                             "</span>"
-            clause_strs.append(clause_str)
-        # Render full formula
-        print("    ", " &and; ".join(clause_strs))
-        print(f"    <p>Clause SAT Count: {sat_count}")
-
-    # HTML Document epilogue
-    print(epilogue)
-
-
+        
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog="SAT/MAX-SAT instance and solution generator")
-    parser.add_argument('instance', type=str, help="File with the CNF formula instance in logic program form.")
-    main(parser.parse_args())
+    main() 
